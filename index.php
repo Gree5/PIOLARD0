96ae8d66accede3988ce992aeceb7a1741dd193b
Gree5 <160947838+Gree5@users.noreply.github.com>
2024-08-03 23:07:20 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+// This is your main page file, e.g., index.php
+include 'Views\log.php';  // Includes header content
+
+?>
+
+<main>
+   <div class="d-flex">
+      <div class="w-100">
+         <img src="assets\img\iniciopng.jpg" class="img-fluid w-100" alt="">
+      </div>
+      <div class="w-100 text-center" style="
+    align-content: center;
+">
+         <div style="
+    font-size: 30px;
+    font-weight: bold;
+" class="color01 my-4">
+            Iniciar Sesión
+         </div>
+         <form action="">
+            <div class="px-4 mx-auto" style="
+    width: 50%;
+">
+               <div class="form-floating mb-3">
+                  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                  <label for="floatingInput">Correo Electrónico</label>
+               </div>
+               <div class="form-floating">
+                  <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                  <label for="floatingPassword">Contraseña</label>
+               </div>
+               <div class="col-12 m-3">
+                  <button class="btn bg_web01" type="button" >Ingresar</button>
+               </div>
+            </div>
+         </form>
+         
+      </div>
+    </div>
+   <div class="d-flex">
+   <div class="d-flex w-100 color01 my-5" style="
+    justify-content: center;
+">
+      <div class="mx-5" style="
+    font-size: 30px;
+">
+         seguridad
+      </div>
+      <div style="font-size: 30px;" class="mx-5">
+         Somos una empresa que prioriza la seguridad de sus clientes en cada uno de nuestros viajes
+      </div>
+   </div>
+   </div>
+</main>
